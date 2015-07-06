1130457cbb3f498bd72d1ba7688828cebf1cfe17
Robert Wolf <robert.wolf@wooga.com>
2015-07-06 17:06:57 +0200

    fixed intendation

--- models/question.rb
+++ models/question.rb
@@ -2,6 +2,12 @@ class Question < ActiveRecord::Base
   belongs_to :round
   has_many :votes
 
+  CurrentUserJoinCondition = <<-SQL
+    LEFT JOIN votes v1 ON (
+      v1.question_id = questions.id AND v1.hashed_mail = %{hashed_mail}
+    )
+  SQL
+
   def self.create_question_with_vote(params, mail)
     Question.transaction do
       Question.create(params).vote(1, mail)
@@ -9,11 +15,6 @@ class Question < ActiveRecord::Base
   end
 
   def self.join_vote_info(round, mail, filter)
-    current_user_join_condition = <<-SQL
-      LEFT JOIN votes v1 ON (
-        v1.question_id = questions.id AND v1.hashed_mail = %{hashed_mail}
-      )
-    SQL
     query = all.select("questions.*")
       .select("rank() over (ORDER BY COALESCE(sum(v.vote), 0) desc) as rank")
       .select("COALESCE(sum(v.vote), 0) as score")
@@ -23,7 +24,7 @@ class Question < ActiveRecord::Base
       .select("count(NULLIF(v.vote, -1)) as up_votes")
       .select("max(v1.vote) as myvote")
       .joins("left join votes v on (v.question_id = questions.id)")
-      .joins(current_user_join_condition % {
+      .joins(CurrentUserJoinCondition % {
         :hashed_mail => ActiveRecord::Base.sanitize(round.hashed_mail(mail))
       })
       .where(:round => round)
@@ -39,14 +40,14 @@ class Question < ActiveRecord::Base
   end
 
   def vote(value, mail)
-    vote =  Vote.where(:hashed_mail => round.hashed_mail(mail), :question => self).first_or_create
+    vote = Vote.where(:hashed_mail => round.hashed_mail(mail), :question => self).first_or_create
     vote.vote = value
     vote.save
   end
 
   def voted?(mail = nil)
     aggregated_value(:voted) do
-       votes.any? {|vote| vote.hashed_mail == round.hashed_mail(mail)}
+      votes.any? {|vote| vote.hashed_mail == round.hashed_mail(mail)}
     end
   end
 
