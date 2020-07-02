a2e5485e2486449a8e2457af87d2393a0d12e43d
Mariano Vallés <zucaritas@gmail.com>
2020-07-02 12:10:48 +0200

    Add correct session user info

--- lib/wham_helper.rb
+++ lib/wham_helper.rb
@@ -1,10 +1,10 @@
 module WhamHelper
   def current_user
-    request.session['email']
+    request.session[:user][:user][:email]
   end
 
   def current_user_name
-    request.session['username'].to_s.split(' ').map{|name| name.capitalize}.join(' ')
+    "#{request.session[:user][:user][:first_name]} #{request.session[:user][:user][:last_name]}"
   end
 
   def current_page(page)
